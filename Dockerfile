f59cc36fd6c7725271bf4221afc94e96feaa1b58
KT Kang <kt.kang@ridi.com>
2018-12-19 12:35:13 +0900

    Change opcache option

--- Dockerfile
+++ Dockerfile
@@ -1,48 +1,38 @@
-FROM ubuntu:xenial
+FROM php:7.2.13-apache-stretch
 
-# 카카오 미러 서버로 저장소 변경
+ENV DEBIAN_FRONTEND=noninteractive
+
 RUN sed -i -e "s/\/\/archive\.ubuntu/\/\/mirror\.kakao/" /etc/apt/sources.list
 
-RUN apt-get update --fix-missing && apt-get install -y \
-    software-properties-common \
-    curl \
-&& LC_ALL=C.UTF-8 apt-add-repository -y ppa:ondrej/php
+RUN pecl install xdebug-2.6.0 \
+&& docker-php-ext-install pdo_mysql \
+&& docker-php-ext-enable \
+    opcache \
+    xdebug \
+&& docker-php-source delete && rm -rf /tmp/*
 
-RUN apt-get update --fix-missing && apt-get install --no-install-recommends -y \
-    # envsubst 사용을 위해 필요
-    gettext-base \
-    apache2 \
-    build-essential \
-    libgmp-dev \
-    php7.1 \
-    php7.1-cli \
-    php7.1-curl \
-    php7.1-mbstring \
-    php7.1-mcrypt \
-    php7.1-mysql \
-    php7.1-apcu \
-    php7.1-zip \
-    php7.1-bcmath \
-    libapache2-mod-php7.1 \
-    php-bcmath \
-    php-xdebug \
-    # install pecl
-    php7.1-dev \
-    php7.1-xml \
-    php-pear
+RUN mkdir -p /usr/local/etc/php/apache2 \
+&& cp -R /usr/local/etc/php/conf.d /usr/local/etc/php/apache2 \
+&& echo 'export PHP_INI_SCAN_DIR=/usr/local/etc/php/apache2/conf.d' >> /etc/apache2/envvars
 
-COPY php/apache.ini /etc/php/7.1/apache2/php.ini
-COPY php/cli.ini /etc/php/7.1/cli/php.ini
+COPY php/cli.ini /usr/local/etc/php/php.ini
+COPY php/apache.ini /usr/local/etc/php/apache2/apache.ini
 
-# enable apache modules
-RUN a2enmod rewrite expires headers proxy proxy_http
+ENV PHP_TIMEZONE="Asia/Seoul" \
+    PHP_OPCACHE_ENABLE="0" \
+    PHP_OPCACHE_VALIDATE_TIMESTAMPS="0" \
+    PHP_OPCACHE_MAX_ACCELERATED_FILES="10000" \
+    PHP_OPCACHE_MEMORY_CONSUMPTION="192" \
+    PHP_OPCACHE_MAX_WASTED_PERCENTAGE="10" \
+    PHP_XDEBUG_ENABLE="0" \
+    PHP_XDEBUG_REMOTE_HOST="host.docker.internal" \
+    APACHE_DOC_ROOT="/app/public"
 
-ENV APACHE_DOC_ROOT /var/www/html
-
-COPY ./index.php /var/www/html/index.php
-COPY ./health.php /var/www/html/health.php
+RUN a2enmod rewrite
+COPY ./index.php /app/public/index.php
+COPY ./health.php /app/public/health.php
 
 # Change entrypoint
 COPY ./docker-entrypoint.sh /
 ENTRYPOINT ["/docker-entrypoint.sh"]
-CMD ["apache2ctl", "-D", "FOREGROUND"]
+CMD ["apache2-foreground"]
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,14 +2,47 @@
 
 set -e
 
-# Configure Apache Document Root
+# Configure PHP opcache
+if [[ ! -z "${PHP_OPCODE_ENABLE}" ]]
+then
+    # Note: If you want to use Xdebug and OPCache together, you must load Xdebug after OPCache.
+    PHP_OPCODE_INI_PATH=/usr/local/etc/php/conf.d/docker-php-ext-opcache.ini
+    echo "opcache.enable=1" >> ${PHP_OPCODE_INI_PATH}
+    echo "opcache.enable_cli=1" >> ${PHP_OPCODE_INI_PATH}
+    echo "opcache.validate_timestamps=${PHP_OPCACHE_VALIDATE_TIMESTAMPS}" >> ${PHP_OPCODE_INI_PATH}
+    echo "opcache.memory_consumption=${PHP_OPCACHE_MEMORY_CONSUMPTION}" >> ${PHP_OPCODE_INI_PATH}
+    echo "opcache.max_accelerated_files=${PHP_OPCACHE_MAX_ACCELERATED_FILES}" >> ${PHP_OPCODE_INI_PATH}
+    echo "opcache.max_wasted_percentage=${PHP_OPCACHE_MAX_WASTED_PERCENTAGE}" >> ${PHP_OPCODE_INI_PATH}
+    echo "opcache.interned_strings_buffer=16" >> ${PHP_OPCODE_INI_PATH}
+fi
+
+# Configure PHP xdebug
+if [[ ! -z "${PHP_XDEBUG_ENABLE}" ]]
+then
+    PHP_XDEBUG_INI_PATH=/usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
+    echo "xdebug.remote_enable=1" >> ${PHP_XDEBUG_INI_PATH}
+    echo "xdebug.remote_host=${PHP_XDEBUG_REMOTE_HOST}" >> ${PHP_XDEBUG_INI_PATH}
+    echo "xdebug.remote_port=9000" >> ${PHP_XDEBUG_INI_PATH}
+    echo "xdebug.default_enable=1" >> ${PHP_XDEBUG_INI_PATH}
+    echo "xdebug.coverage_enable=1" >> ${PHP_XDEBUG_INI_PATH}
+    echo "xdebug.profiler_enable_trigger=1" >> ${PHP_XDEBUG_INI_PATH}
+    echo "xdebug.profiler_output_name=xdebug-profile-cachegrind.out-%H-%R" >> ${PHP_XDEBUG_INI_PATH}
+    echo "xdebug.var_display_max_children=128" >> ${PHP_XDEBUG_INI_PATH}
+    echo "xdebug.var_display_max_data=2048" >> ${PHP_XDEBUG_INI_PATH}
+    echo "xdebug.var_display_max_depth=128" >> ${PHP_XDEBUG_INI_PATH}
+    echo "xdebug.max_nesting_level=200" >> ${PHP_XDEBUG_INI_PATH}
+fi
+
+# Configure Apache document root
 mkdir -p ${APACHE_DOC_ROOT}
 chown www-data:www-data ${APACHE_DOC_ROOT}
 sed -i "s|DocumentRoot /var/www/html\$|DocumentRoot ${APACHE_DOC_ROOT}|" /etc/apache2/sites-available/000-default.conf
-echo "<Directory ${APACHE_DOC_ROOT}>" > /etc/apache2/conf-available/document-root-directory.conf
-echo "  AllowOverride All" >> /etc/apache2/conf-available/document-root-directory.conf
-echo "  Require all granted" >> /etc/apache2/conf-available/document-root-directory.conf
-echo "</Directory>" >> /etc/apache2/conf-available/document-root-directory.conf
+
+APACHE_DOC_ROOT_CONFI_PATH=/etc/apache2/conf-available/document-root-directory.conf
+echo "<Directory ${APACHE_DOC_ROOT}>" > ${APACHE_DOC_ROOT_CONFI_PATH}
+echo "  AllowOverride All" >> ${APACHE_DOC_ROOT_CONFI_PATH}
+echo "  Require all granted" >> ${APACHE_DOC_ROOT_CONFI_PATH}
+echo "</Directory>" >> ${APACHE_DOC_ROOT_CONFI_PATH}
 a2enconf "document-root-directory.conf"
 
 exec "$@"
